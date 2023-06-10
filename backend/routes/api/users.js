35318456857d40b700dc4e5be68057360b06067c
carveyh <carvey.hor@gmail.com>
2023-06-10 14:19:23 -0400

    Create tweets route, remove root index route and public HTML serving folder:

---
+++ backend/routes/api/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  // res.send('Respond with a user resource bruhhh');
+  // res.json() send response with a JSON body.
+  res.json({
+    message: "GET /api/users"
+  })
+});
+
+module.exports = router;
