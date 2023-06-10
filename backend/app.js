9d5981bc021196c2b45c89943b18790c6cfa4331
carveyh <carvey.hor@gmail.com>
2023-06-10 15:05:43 -0400

    Error handling logic add

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const { isProduction } = require('./config/keys');
 // CSRF Protection
 const csurf = require('csurf');
 
+// Error handling
+const debug = require('debug');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
@@ -38,5 +41,26 @@ app.use(
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.statusCode = 404;
+  next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+  })
+});
 
 module.exports = app;
